bfb26604efd9ca0e37a07297b9773d2acc9e0ccd
answershuto <905307789@qq.com>
2016-11-26 17:23:51 +0800

    init mongoDB

---
+++ src/cfg.js
@@ -0,0 +1,3 @@
+export default {
+	mongodb: 'mongodb://localhost/cfg'
+}
--- src/mongodbLib.js
+++ src/mongodbLib.js
@@ -1,11 +1,24 @@
+// ES6 config lib
+
+import cfg from './cfg'              
+
+let mongoose = require('mongoose');
+let db = mongoose.connect(cfg.mongodb);
+/*笔记表*/
+let Schema = new mongoose.Schema({
+	name: String,
+	config: Object
+})
+
+let mgCfg = mongoose.model('config', Schema);
 
 
-let db = (function(){
+let DBoperation = (function(){
 	return {
 		test(){
-			console.log('mongodb lib test');
+			console.log('mongoDB operation lib test');
 		}
 	}
 })();
 
-export default db;
+export default DBoperation;
